4fe45e12c0a2275ec153f1415cc80a8874e448a9
rn1024 <benstyle1024@gmail.com>
2024-01-27 00:22:44 +0800

    feat: 更换版权

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'cosmos';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
